cdded02729c51c7a1935f01fc0710caf678222c6
Евгений <geoche74@gmail.com>
2022-10-12 11:52:47 +0500

    Исправил недочеты

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -5,30 +5,32 @@ User = get_user_model()
 
 
 class Group(models.Model):
-    title = models.CharField(verbose_name="Название группы", max_length=200)
-    slug = models.SlugField(verbose_name="Адрес страницы группы", unique=True)
-    description = models.TextField(verbose_name="Описание группы")
+    title = models.CharField(verbose_name="Название", max_length=200)
+    slug = models.SlugField(verbose_name="Адрес страницы", unique=True)
+    description = models.TextField(verbose_name="Описание")
 
     def __str__(self):
         return self.title
 
 
 class Post(models.Model):
-    text = models.TextField(verbose_name="Название статьи")
+    text = models.TextField(verbose_name="Содержание статьи")
     pub_date = models.DateTimeField(
         verbose_name="Время публикации", auto_now_add=True
     )
     author = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
-        related_name='posts'
+        related_name='posts',
+        verbose_name="Автор"
     )
     group = models.ForeignKey(
         Group,
         on_delete=models.SET_NULL,
         blank=True,
         null=True,
-        related_name='posts'
+        related_name='posts',
+        verbose_name="Группа"
     )
 
     class Meta:
--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -15,7 +15,7 @@ def index(request):
 
 def group_posts(request, slug):
     group = get_object_or_404(Group, slug=slug)
-    posts = Post.objects.filter(group=group).order_by('-pub_date')[:LIMIT]
+    posts = group.posts.all()[:LIMIT]
     context = {
         'group': group,
         'posts': posts,
